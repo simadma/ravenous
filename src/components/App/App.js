5e44851071efb93bb2f3a12f2e6b570c265bf498
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-02-24 08:57:23 +0100

    Build Search Bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import styles from "./App.module.css";
 
 import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
 
 const App = () => (
   <div className={styles.App}>
     <h1>ravenous</h1>
+    <SearchBar />
     <BusinessList />
   </div>
 );
