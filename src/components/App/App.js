d6a872a8b56ec254f467136de45865f1102efb2b
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-02-24 07:16:12 +0100

    Refactor and build the Business component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.css";
+
+import Business from "../Business/Business";
+
+const App = () => (
+  <div className={styles.App}>
+    <h1>ravenous</h1>
+    <Business />
+  </div>
+);
+
+export default App;
---
+++ src/components/Business/Business.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./Business.module.css";
+
+const business = {
+  imageSrc: "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "Mama Margarita",
+  address: "3214 Pizza Street",
+  city: "Pizza Town",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.9,
+  reviewCount: 89,
+};
+
+const Business = () => (
+  <div className={styles.Business}>
+    <div className={styles.imageContainer}>
+      <img src={business.imageSrc} alt="Delicious Pizza" />
+    </div>
+    <h2>{business.name}</h2>
+    <div className={styles.BusinessInformation}>
+      <div className={styles.BusinessAddress}>
+        <p>{business.address}</p>
+        <p>{business.city}</p>
+        <p>{`${business.state} ${business.zipCode}`}</p>
+      </div>
+      <div className={styles.BusinessReviews}>
+        <h3>{business.category.toUpperCase()}</h3>
+        <h3 className={styles.rating}>{`${business.rating} stars`}</h3>
+        <p>{`${business.reviewCount} reviews`}</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Business;
