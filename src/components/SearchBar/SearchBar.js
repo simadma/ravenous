f2cb2de0952051d25da4babca3ad97034b073574
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-02-25 20:51:20 +0100

    Add State to the SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SearchBar.module.css";
 
 const sortByOptions = {
@@ -8,6 +8,10 @@ const sortByOptions = {
 };
 
 const SearchBar = () => {
+  const [term, setTerm] = useState("");
+  const [location, setLocation] = useState("");
+  const [sortBy, setSortBy] = useState("best_match");
+
   const renderSortByOptions = () => (
     Object.keys(sortByOptions).map(
       sortByOption => {
