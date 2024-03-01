e261a241357f264d602dff9224df1ffa52b01dfb
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-03-01 21:30:12 +0100

    Integrate Yelp API to the App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-
-const business = {
-  imageSrc: "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "Mama Margarita",
-  address: "3214 Pizza Street",
-  city: "Pizza Town",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.9,
-  reviewCount: 89,
-};
-
-const n = 6;
-
-const businesses = new Array(n).fill({...business});
+import Yelp from "../../utils/Yelp";
 
 const App = () => {
-  const searchYelp = (term, location, sortBy) => {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  const [businesses, setBusinesses] = useState([]);
+
+  const searchYelp = async (term, location, sortBy) => {
+    const businesses = await Yelp.search(term, location, sortBy);
+    setBusinesses(businesses);
   };
+
   return (
     <div className={styles.App}>
       <h1>ravenous</h1>
